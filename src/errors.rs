f2711e429c028939c236e965345db8ec0b3939e5
brhutchins <bhutchins@gmail.com>
2025-05-31 13:00:37 +0200

    feat: Serial requests

--- src/errors.rs
+++ src/errors.rs
@@ -1,16 +1,19 @@
-use crate::api_versions::ApiVersionError;
+use crate::{api_versions::ApiVersionError, kafka_request::KafkaRequestError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 #[repr(i16)]
 pub enum KafkaError {
-    #[error("Unspported version")]
+    #[error("Corrupt message")]
+    CorruptMessageError(#[from] KafkaRequestError),
+    #[error("Unsupported version")]
     UnsupportedVersionError(#[from] ApiVersionError),
 }
 
 impl KafkaError {
     pub fn code(&self) -> i16 {
         match self {
+            KafkaError::CorruptMessageError(_) => 2,
             KafkaError::UnsupportedVersionError(_) => 35,
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_stream(stream);
+                let _ = handle_stream(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -29,50 +29,64 @@ fn main() {
     }
 }
 
-fn handle_stream(mut stream: TcpStream) {
-    let mut message_size_buffer = [0; 4];
-    stream
-        .read_exact(&mut message_size_buffer)
-        .expect("Failed to read stream");
-    let message_size = i32::from_be_bytes(
-        message_size_buffer[..]
-            .try_into()
-            .expect("Failed to get message size"),
-    );
-    let mut buffer = vec![0u8; message_size.try_into().expect("usize smaller than i32")];
-    stream
-        .read_exact(&mut buffer[..])
-        .expect("Failed to read stream");
-    let request: KafkaRequest = buffer[..].try_into().expect("Failed to parse KafkaRequest");
-    println!("{:?}", request);
+fn handle_stream(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buffer = bytes::BytesMut::new();
 
-    let mut error_code: i16 = 0;
+    loop {
+        let mut read_buffer = [0u8; 4096];
+        match stream.read(&mut read_buffer) {
+            Ok(0) => break,
+            Ok(n) => buffer.extend_from_slice(&read_buffer[..n]),
+            Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e),
+        };
 
-    match handle_request(&request) {
-        Ok(v) => v,
-        Err(err) => {
-            println!("{:?}", err);
-            error_code = err.code();
+        while buffer.len() >= 4 {
+            let msg_length =
+                i32::from_be_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]) as usize;
+            let total_length = msg_length + 4;
+
+            if buffer.len() < total_length {
+                // If we haven't receive the whole message yet, we'll break to the next iteration
+                // of the loop, and try again.
+                break;
+            }
+
+            let msg = buffer.split_to(total_length);
+            let request: KafkaRequest = msg[4..].try_into().expect("Failed to parse KafkaRequest");
+            println!("{:?}", request);
+
+            let mut error_code: i16 = 0;
+
+            match handle_request(&request) {
+                Ok(v) => v,
+                Err(err) => {
+                    println!("{:?}", err);
+                    error_code = err.code();
+                }
+            };
+            let response_header = KafkaResponseHeaderV0 {
+                correlation_id: request.correlation_id,
+            };
+
+            let response_body = api_versions::api::ResponseBody::new(error_code);
+
+            let response = KafkaResponse {
+                header: KafkaResponseHeader::KafkaResponseHeaderV0(response_header),
+                body: KafkaResponseBody::ApiVersions(response_body),
+            };
+
+            println!("Response: {:?}", response);
+
+            let response_bytes: Vec<u8> = response.into();
+
+            println!("Response bytes: {:02x?}", response_bytes);
+
+            stream.write_all(&response_bytes).unwrap();
         }
-    };
-    let response_header = KafkaResponseHeaderV0 {
-        correlation_id: request.correlation_id,
-    };
+    }
 
-    let response_body = api_versions::api::ResponseBody::new(error_code);
-
-    let response = KafkaResponse {
-        header: KafkaResponseHeader::KafkaResponseHeaderV0(response_header),
-        body: KafkaResponseBody::ApiVersions(response_body),
-    };
-
-    println!("Response: {:?}", response);
-
-    let response_bytes: Vec<u8> = response.into();
-
-    println!("Response bytes: {:02x?}", response_bytes);
-
-    stream.write_all(&response_bytes).unwrap();
+    Ok(())
 }
 
 fn handle_request(request: &KafkaRequest) -> Result<(), KafkaError> {
